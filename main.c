ee82670faed1f0aa5ca148e7f1b029fb8f0a4922
Antoine POUCHET <apouchet@e1r8p8.42.fr>
2019-10-08 12:35:33 +0200

    mandelbrot start

--- main.c
+++ main.c
@@ -23,6 +23,41 @@ typedef struct	s_picture
 	int			endian;
 }				t_picture;
 
+int		ft_mandelbrot(t_data *data)
+{
+int iteration_max = 50;
+
+// on calcule la taille de l'image :
+double zoom_x = FENETRE/(0.6 + 2.1) ;
+double zoom_y = FENETRE/(1.2 + 1.2) ;
+
+	for (int x = 0; x < FENETRE; x++)
+	{
+		for (int y = 0; y < FENETRE; y++)
+		{
+			double c_r = x / zoom_x - 2.1;
+			double c_i = y / zoom_y - 1.2;
+			double z_r = 0;
+			double z_i = 0;
+			int i = 0;
+
+			do
+			{
+				double tmp = z_r;
+				z_r = z_r * z_r - z_i * z_i + c_r;
+				z_i = 2 * z_i * tmp + c_i;
+				i++;
+			}
+			while (z_r * z_r + z_i * z_i < 4 && i < iteration_max);
+			// printf("i = %d\n", i);
+			if (i == iteration_max)
+				data->image[4 * FENETRE * y + 4 * x] = 0xFF;
+		}
+	}
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->p_image, 0, 0);
+	return (0);
+}
+
 int		ft_affich(t_data *data)
 {
 	int x;
@@ -79,7 +114,7 @@ int		ft_key(int key,	t_data *data)
 	printf("key = %d\n", key);
 	if (key == 53)
 		exit(0);
-	ft_affich(data);
+	ft_mandelbrot(data);
 	return (1);
 }
 
@@ -96,7 +131,7 @@ int		main(int argc, char **argv)
 	// ft_base(argv[1], &val);
 	// mlx_hook(val.mlx[1], 2, 0, &ft_key_on, &val);
 	mlx_key_hook(data.win_ptr, &ft_key, &data);
-	mlx_loop_hook(data.mlx_ptr, &ft_affich, &data);
+	mlx_loop_hook(data.mlx_ptr, &ft_mandelbrot, &data);
 	mlx_loop(data.mlx_ptr);
 	return (0);
 }
